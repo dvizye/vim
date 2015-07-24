d06688db3dcbb83a9b5830d90a134d4863fa980e
David Ye <dvizye@gmail.com>
2015-07-23 22:19:12 -0500

    Tex file detection

--- ftdetect/tex.vim
+++ ftdetect/tex.vim
@@ -1,3 +1,4 @@
+au BufRead,BufNewFile *.tex set filetype=tex
 " au BufWritePost *.tex :Latex
 vnoremap <c-w> :Latex<cr>
 nnoremap <c-w> :Latex<cr>
--- setup.sh
+++ setup.sh
@@ -1,3 +1,5 @@
 SCRIPTPATH=$( cd $(dirname $0) ; pwd -P )
 ln -s $SCRIPTPATH ~/.vim
 ln -s $SCRIPTPATH/vimrc.vim ~/.vimrc
+
+ln -s $SCRIPTPATH/vimrc.vim ~/.ideavimrc
