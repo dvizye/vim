f9a9973f3cf6de3540605efc9e3c4f631ffb2997
David Ye <dvizye@gmail.com>
2016-04-25 17:20:00 -0700

    Add undodir

--- setup.sh
+++ setup.sh
@@ -2,6 +2,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 if [[ ! -d "$HOME/.vim" ]]; then
     ln -s $DIR ~/.vim
     ln -s $DIR/vimrc.vim ~/.vimrc
+    mkdir $DIR/undodir
     # For IntelliJ vim-mode
     # ln -s $DIR/vimrc.vim ~/.ideavimrc
     "Symlinked vim configuration"
